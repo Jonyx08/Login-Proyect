45b2b713a0c1e7f06a4c830a07d80e9019ec8326
Jonyx08 <jonyt.delao@gmail.com>
2025-07-06 01:43:17 -0600

    fix

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.set("view engine", "hbs")
 app.set("views",tempelatePath)
 app.use(express.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname, "public")))
+// Serve static files
+app.use(express.static('public'));
 
 
 
