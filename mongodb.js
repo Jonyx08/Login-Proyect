7d5da2456c249874459630dd70263cf3ecae29c6
Jonyx08 <jonyt.delao@gmail.com>
2025-07-06 00:34:05 -0600

    fixing-again

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI)
+// Debug lines - remove after fixing
+console.log('DB_URL:', process.env.DB_URL);
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+console.log('Available env vars:', Object.keys(process.env).filter(key => key.includes('DB') || key.includes('MONGO')));
+
+if (!process.env.DB_URL) {
+  console.error('DB_URL environment variable is not set!');
+  process.exit(1);
+}
+
+mongoose.connect(process.env.DB_URL)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Connection error:', err));
-
+  
 const LogInSchema=new mongoose.Schema({
     name:{
         type:String,
